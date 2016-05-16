d7fdff161ef4ee097ea668a8e789cfaf36e5d53b
Jonas Möller <github@uelfte.ninja>
2016-05-16 03:17:17 +0200

    fixing test

--- BeerTest.php
+++ BeerTest.php
@@ -5,7 +5,21 @@ class BeerTest extends \PHPUnit_Framework_TestCase
     public function test_serialization()
     {
         $Beer = new rauhkrusche\BeerPHP\Beer;
-        //$this->expectOutputString('µµµµµ∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫_µµµµµ∫Eµµµµµµµµµµµµ∫µµµµµ∫-BEERBEERBEERBEERBEER∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫-BEERBEER-BEER∫BEER_BEER∫');
-        print $Beer->serialize('Test_TEST-test-BEER.,');
+        $this->expectOutputString('µµµµµ∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫');
+        print $Beer->serialize('Test');
+    }
+
+    public function test_deserialization()
+    {
+        $Beer = new rauhkrusche\BeerPHP\Beer;
+        $this->expectOutputString('Test');
+        print $Beer->deserialize('µµµµµ∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫');
+    }
+
+    public function test_loop()
+    {
+        $Beer = new rauhkrusche\BeerPHP\Beer;
+        $this->expectOutputString('q.m,BEER-QMer');
+        print $Beer->deserialize($Beer->serialize('q.m,BEER-QMer'));
     }
 }
--- src/Beer/Beer.php
+++ src/Beer/Beer.php
@@ -5,15 +5,14 @@ class Beer
     private $alphabet = array('q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm');
     public function serialize($input)
     {
-        global $alphabet;
         $input = str_replace('.', 'BEER-BEER∫', $input);
         $input = str_replace(',', 'BEER_BEER∫', $input);
 
-        for ($i = 0; $i <= count($alphabet); $i++) {
-            if (!in_array($alphabet[$i], array('b', 'e', 'r'))) {
-                $input = str_replace(strtoupper($alphabet[$i]), $this->repeatString('∫', 'µ', $i), $input);
+        for ($i = 0; $i <= count($this->alphabet); $i++) {
+            if (!in_array($this->alphabet[$i], array('b', 'e', 'r'))) {
+                $input = str_replace(strtoupper($this->alphabet[$i]), $this->repeatString('∫', 'µ', $i), $input);
             }
-            $input = str_replace($alphabet[$i], $this->repeatString('∫', 'BEER', $i), $input);
+            $input = str_replace($this->alphabet[$i], $this->repeatString('∫', 'BEER', $i), $input);
 
         }
         return $input;
@@ -21,15 +20,14 @@ class Beer
 
     public function deserialize($input)
     {
-        global $alphabet;
         $input = str_replace('BEER-BEER∫', '.', $input);
         $input = str_replace('BEER_BEER∫', ',', $input);
 
-        for ($i = count($alphabet) - 1; $i >= 0; $i--) {
-            if (!in_array($alphabet[$i], array('b', 'e', 'r'))) {
-                $input = str_replace($this->repeatString('∫', 'µ', $i), strtoupper($alphabet[$i]), $input);
+        for ($i = count($this->alphabet) - 1; $i >= 0; $i--) {
+            if (!in_array($this->alphabet[$i], array('b', 'e', 'r'))) {
+                $input = str_replace($this->repeatString('∫', 'µ', $i), strtoupper($this->alphabet[$i]), $input);
             }
-            $input = str_replace($this->repeatString('∫', 'BEER', $i), $alphabet[$i], $input);
+            $input = str_replace($this->repeatString('∫', 'BEER', $i), $this->alphabet[$i], $input);
 
         }
         return $input;
