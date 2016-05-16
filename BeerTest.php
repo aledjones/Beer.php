87718ca5ddf186e03d4847a735fa0ba134b82af1
Jonas Möller <github@uelfte.ninja>
2016-05-16 02:57:29 +0200

    fixing test

--- BeerTest.php
+++ BeerTest.php
@@ -1,12 +1,10 @@
 <?php
-include "src/Beer/Beer.php";
-
 
 class BeerTest extends \PHPUnit_Framework_TestCase
 {
     public function test_serialization()
     {
-        $Beer = new Beer;
+        $Beer = new rauhkrusche\BeerPHP\Beer;
         $this->expectOutputString('µµµµµ∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫_µµµµµ∫Eµµµµµµµµµµµµ∫µµµµµ∫-BEERBEERBEERBEERBEER∫BEERBEERBEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫BEERBEERBEERBEERBEER∫-BEERBEER-BEER∫BEER_BEER∫');
         print $Beer->serialize('Test_TEST-test-BEER.,');
     }
